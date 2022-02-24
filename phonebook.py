ba225a35b59dd08487c3dfa4f127979a7cf9148b
Franklin Simpson <simpson.franklin44@gmail.com>
2022-02-24 11:56:25 -0500

    func1

--- phonebook.py
+++ phonebook.py
@@ -8,4 +8,11 @@ def menu():
     print("what would you like to do (1-5)?")
 
 menu()    
-  
+PB = {}
+while True:
+    n=int(input("enter number [1-5]:-"))
+    if n ==1:
+        name=input("enter name:-")
+        pn=(input("enter phone number:-"))
+        
+        break
