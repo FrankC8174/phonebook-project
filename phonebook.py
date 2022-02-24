fc51da6470b5b9fd755e458fd216cef673e2b0f6
Franklin Simpson <simpson.franklin44@gmail.com>
2022-02-24 11:26:07 -0500

    menu

--- phonebook.py
+++ phonebook.py
@@ -1,1 +1,11 @@
+def menu():
+    print("-------Lil Black Book--------")
+    print("[1] Look up an entry")
+    print("[2] Set an entry")
+    print("[3] delete an entry")
+    print("[4] List all entries")
+    print("[5] Quit")
+    print("what would you like to do (1-5)?")
 
+menu()    
+  
